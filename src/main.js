dd635f84a93ca969307a5ae9b4e461aff04230ed
Samarth AGARWAL <samu.agarwal@gmail.com>
2013-12-23 10:53:18 +0100

    Remove main.css, Add 100ms grace period between presses

--- src/main.js
+++ src/main.js
@@ -5,6 +5,10 @@ xtag.customEvents.press = {
         "mouseup"
     ],
     condition: function (event) {
+      if (event.target.xtag.customevent.press.data.activated) return false;
+      setTimeout(function () {
+       this.activated=false;
+        }.bind(event.target.xtag.customevents.press.data), 100);
       return true;
     }
 }
