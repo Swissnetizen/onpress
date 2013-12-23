b63efdea5b28717015e6f6e1183bad507fdb878d
Samarth AGARWAL <samu.agarwal@gmail.com>
2013-12-23 11:11:58 +0100

    Define customevent object in element

--- src/main.js
+++ src/main.js
@@ -5,6 +5,9 @@ xtag.customEvents.press = {
         "mouseup"
     ],
     condition: function (event) {
+      if (!event.target.xtag) event.target.xtag = {};
+      if (!event.target.xtag.customevent) event.target.xtag.customevent = {};
+      if (!event.target.xtag.customevent.press) event.target.xtag.customevent.press = {data: {activated: false}};
       if (event.target.xtag.customevent.press.data.activated) return false;
       setTimeout(function () {
        this.activated=false;
