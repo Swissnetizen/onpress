783ddc9c24a34daeb7ce22a8ad036d6f9de113eb
Samarth AGARWAL <samu.agarwal@gmail.com>
2013-12-23 11:50:36 +0100

    Set activated to true

--- src/main.js
+++ src/main.js
@@ -12,7 +12,7 @@ xtag.customEvents.press = {
       setTimeout(function () {
        this.activated=false;
         }.bind(event.target.xtag.customevents.press.data), 100);
-      this.activated = true;
+      event.target.xtag.customevents.press.data.activated = true;
       return true;
     }
 }
