e66db7846392d72802b591fd7c2e7cef19edcbfd
Samarth AGARWAL <samu.agarwal@gmail.com>
2014-02-02 20:33:48 +0100

    Add bases of tests

---
+++ spec/onpress.js
@@ -0,0 +1,13 @@
+(function () {
+  describe(function ("onpress") {
+    var el;
+    beforeEach(function () {
+      el = document.createElement("button");
+      el.addEventListener("onpress", function () {
+    });
+    it("should fire when touched")
+    it("should fire when clicked")
+    it("shouldn't fire when held too long")
+    });
+  });
+}());
